cda0e45a9e1fb93a68f05be575660ded3026793c
Liloche19 <lilian.locher@epitech.eu>
2025-02-26 17:19:14 +0100

    [+] user can now choose the interval time

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,10 @@ use std::{
     time::{Duration, Instant},
 };
 
-fn main() {
-    let default_interval_time = 10;
-    let args: Vec<String> = std::env::args().collect();
-    let port: u32 = match args.len() {
-        2 => 80,
-        3 => match args[2].parse() {
-            Ok(value) => value,
-            Err(_) => {
-                println!("Invalid argument was given !");
-                exit(84)
-            }
-        },
-        _ => {
-            println!("Usage: {} <host> <port>", args[0]);
-            exit(84)
-        }
-    };
+fn my_tcping(ip: &str, interval: u64, port: u16) {
     loop {
-        let mut stream = TcpStream::connect(format!("{}:{}", args[1], port))
-            .expect("Error connecting to address!");
+        let mut stream =
+            TcpStream::connect(format!("{}:{}", ip, port)).expect("Error connecting to address!");
         let now = Instant::now();
         stream.write_all(&[1]).unwrap();
         let mut buffer: [u8; 1] = [0; 1];
@@ -33,11 +17,44 @@ fn main() {
         let elapsed = now.elapsed();
         println!(
             "Reply from {} ({}) on port {} took {:?}",
-            args[1],
+            ip,
             stream.peer_addr().unwrap(),
             port,
             elapsed
         );
-        sleep(Duration::from_millis(default_interval_time));
+        sleep(Duration::from_millis(interval));
     }
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let port: u16 = match args.len() {
+        2 | 3 => 80,
+        4 => match args[3].parse() {
+            Ok(value) => value,
+            Err(_) => {
+                println!("Invalid argument was given !");
+                exit(84)
+            }
+        },
+        _ => {
+            println!("Usage: {} <host> [<interval> <port>]", args[0]);
+            exit(84)
+        }
+    };
+    let interval_time: u64 = match args.len() {
+        2 => 1000,
+        3 | 4 => match args[2].parse() {
+            Ok(value) => value,
+            Err(_) => {
+                println!("Invalid argument was given !");
+                exit(84)
+            }
+        },
+        _ => {
+            println!("Usage: {} <host> [<interval> <port>]", args[0]);
+            exit(84)
+        }
+    };
+    my_tcping(&args[1], interval_time, port);
 }
