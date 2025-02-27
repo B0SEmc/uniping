138284577b7cba83da666a37ff547c28225131a7
Liloche19 <lilian.locher@epitech.eu>
2025-02-27 09:19:28 +0100

    [+] parser improved + DEFAULT variables set

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use std::{
     time::{Duration, Instant},
 };
 
+const DEFAULT_PORT: u16 = 80;
+const DEFAULT_INTERVAL_MS: u64 = 1000;
+
 fn my_tcping(ip: &str, interval: u64, port: u16) {
     loop {
         let mut stream =
@@ -28,33 +31,40 @@ fn my_tcping(ip: &str, interval: u64, port: u16) {
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let port: u16 = match args.len() {
-        2 | 3 => 80,
-        4 => match args[3].parse() {
-            Ok(value) => value,
-            Err(_) => {
-                println!("Invalid argument was given !");
-                exit(84)
+    let ip: &str = &args[1];
+    let mut port: u16 = DEFAULT_PORT;
+    let mut interval_ms: u64 = DEFAULT_INTERVAL_MS;
+    match args.len() {
+        2 => (),
+        3 => {
+            interval_ms = match args[2].parse() {
+                Ok(value) => value,
+                Err(_) => {
+                    println!("Invalid argument was given !");
+                    exit(84)
+                }
             }
-        },
+        }
+        4 => {
+            port = match args[3].parse() {
+                Ok(value) => value,
+                Err(_) => {
+                    println!("Invalid argument was given !");
+                    exit(84)
+                }
+            };
+            interval_ms = match args[2].parse() {
+                Ok(value) => value,
+                Err(_) => {
+                    println!("Invalid argument was given !");
+                    exit(84)
+                }
+            }
+        }
         _ => {
             println!("Usage: {} <host> [<interval> <port>]", args[0]);
             exit(84)
         }
-    };
-    let interval_time: u64 = match args.len() {
-        2 => 1000,
-        3 | 4 => match args[2].parse() {
-            Ok(value) => value,
-            Err(_) => {
-                println!("Invalid argument was given !");
-                exit(84)
-            }
-        },
-        _ => {
-            println!("Usage: {} <host> [<interval> <port>]", args[0]);
-            exit(84)
-        }
-    };
-    my_tcping(&args[1], interval_time, port);
+    }
+    my_tcping(ip, interval_ms, port);
 }
