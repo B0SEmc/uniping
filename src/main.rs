71f2e952a97f6104a45a5990bdb108d34abb735c
Liloche19 <lilian.locher@epitech.eu>
2025-02-27 16:16:44 +0100

    [+] status print moved in another function

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,25 @@ use std::{
 const DEFAULT_PORT: u16 = 80;
 const DEFAULT_INTERVAL_MS: u64 = 1000;
 
+fn print_stats(results: &Vec<u128>, ip: &str) {
+    let max = *results.iter().max().unwrap() as f64;
+    let min = *results.iter().min().unwrap() as f64;
+    let len = results.len() as u128;
+    let stats = format!(
+        "Packets: {} min: {:.3} avg: {:.3} max: {:.3} maxdev: {:.3}",
+        len,
+        min / 1000.0,
+        (results.iter().sum::<u128>() / len) as f64 / 1000.0,
+        max / 1000.0,
+        (max - min) / 1000.0
+    );
+    println!();
+    (0..stats.len() / 2 - 11 - ip.len() / 2).for_each(|_| print!("-"));
+    print!(" {} uniping statistics ", ip);
+    (0..stats.len() / 2 - 11 - ip.len() / 2).for_each(|_| print!("-"));
+    println!("\n{}", stats);
+}
+
 fn my_tcping(ip: &str, interval: u64, port: u16) {
     let mut results: Vec<u128> = Vec::new();
     let (tx, rx) = channel::<bool>();
@@ -35,22 +54,7 @@ fn my_tcping(ip: &str, interval: u64, port: u16) {
             exit = true;
         }
     }
-    let max = *results.iter().max().unwrap() as f64;
-    let min = *results.iter().min().unwrap() as f64;
-    let len = results.len() as u128;
-    let stats = format!(
-        "Packets: {} min: {:.3} avg: {:.3} max: {:.3} maxdev: {:.3}",
-        len,
-        min / 1000.0,
-        (results.iter().sum::<u128>() / len) as f64 / 1000.0,
-        max / 1000.0,
-        (max - min) / 1000.0
-    );
-    println!();
-    (0..stats.len() / 2 - 11 - ip.len() / 2).for_each(|_| print!("-"));
-    print!(" {} uniping statistics ", ip);
-    (0..stats.len() / 2 - 11 - ip.len() / 2).for_each(|_| print!("-"));
-    println!("\n{}", stats);
+    print_stats(&results, ip);
 }
 
 fn main() {
