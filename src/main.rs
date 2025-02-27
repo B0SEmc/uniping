5dd8040fa47521a29dbaf71c9f3e3ade237c7b8a
thibaut <thibaut.musslin@epitech.eu>
2025-02-27 16:09:48 +0100

    Add ctrlc handling

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{
     io::{Read, Write},
     net::TcpStream,
     process::exit,
-    thread::sleep,
+    sync::mpsc::channel,
     time::{Duration, Instant},
 };
 
@@ -11,7 +11,11 @@ const DEFAULT_INTERVAL_MS: u64 = 1000;
 
 fn my_tcping(ip: &str, interval: u64, port: u16) {
     let mut results: Vec<u128> = Vec::new();
-    loop {
+    let (tx, rx) = channel::<bool>();
+    ctrlc::set_handler(move || tx.send(true).expect("Could not send signal on channel."))
+        .expect("Error setting Ctrl-C handler");
+    let mut exit = false;
+    while !exit {
         let mut stream =
             TcpStream::connect(format!("{}:{}", ip, port)).expect("Error connecting to address!");
         let now = Instant::now();
@@ -27,22 +31,26 @@ fn my_tcping(ip: &str, interval: u64, port: u16) {
             port,
             ((elapsed.as_micros() as u32) as f64) / 1000.0
         );
-        sleep(Duration::from_millis(interval));
-        if false {
-            break;
+        if rx.recv_timeout(Duration::from_millis(interval)).is_ok() {
+            exit = true;
         }
     }
     let max = *results.iter().max().unwrap() as f64;
     let min = *results.iter().min().unwrap() as f64;
     let len = results.len() as u128;
-    println!(
-        "Packets: {} min/avg/max/maxdev {:.3}/{:.3}/{:.3}/{:.3}",
+    let stats = format!(
+        "Packets: {} min: {:.3} avg: {:.3} max: {:.3} maxdev: {:.3}",
         len,
         min / 1000.0,
         (results.iter().sum::<u128>() / len) as f64 / 1000.0,
         max / 1000.0,
         (max - min) / 1000.0
-    )
+    );
+    println!();
+    (0..stats.len() / 2 - 11 - ip.len() / 2).for_each(|_| print!("-"));
+    print!(" {} uniping statistics ", ip);
+    (0..stats.len() / 2 - 11 - ip.len() / 2).for_each(|_| print!("-"));
+    println!("\n{}", stats);
 }
 
 fn main() {
