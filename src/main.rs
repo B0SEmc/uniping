bd6636d08ac771775c4564cc9d401d777aad8210
Liloche19 <lilian.locher@epitech.eu>
2025-03-02 14:27:48 +0100

    [+] flags added for the parser

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,24 @@ use tcp::my_tcping;
 const DEFAULT_PORT: u16 = 80;
 const DEFAULT_INTERVAL_MS: u64 = 1000;
 
+pub fn print_help() {
+    println!("USAGE:\n\t./uniping TARGET [[flags] [args]]");
+    println!("\nDESCRIPTION:");
+    println!("\t-h | --help\tPrint this menu.");
+    println!("\t-q | --quiet\tDoes not print the result of each requests.");
+    println!("\t-p | --port\tChoose a specific port to ping.");
+    println!("\t-i | --interval\tChoose a specific interval time between each requests.");
+    println!("\t-d\t\tSecret opton.");
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let mut settings = Args {
-        ip: &args[1],
+        ip: "",
         port: DEFAULT_PORT,
         interval: DEFAULT_INTERVAL_MS,
+        d_flag: false,
+        quiet: false,
     };
     let mut results: Vec<u128> = Vec::new();
     parse(&mut settings, &args);
--- src/parser.rs
+++ src/parser.rs
@@ -1,42 +1,74 @@
 use std::process::exit;
 
+use crate::print_help;
+
 pub struct Args<'a> {
     pub ip: &'a str,
     pub port: u16,
     pub interval: u64,
+    pub d_flag: bool,
+    pub quiet: bool,
 }
 
-pub fn parse(settings: &mut Args, args: &[String]) {
-    match args.len() {
-        2 => (),
-        3 => {
-            settings.interval = match args[2].parse() {
-                Ok(value) => value,
-                Err(_) => {
-                    println!("Invalid argument was given !");
-                    exit(84)
+pub fn parse<'a>(settings: &mut Args<'a>, args: &'a [String]) {
+    let mut i = 1;
+    while i < args.len() {
+        match args[i].as_str() {
+            "-d" => {
+                settings.d_flag = true;
+                settings.interval = 1
+            }
+            "-h" | "--help" => {
+                print_help();
+                exit(0);
+            }
+            "-q" | "--quiet" => settings.quiet = true,
+            "-p" | "--port" => {
+                if i + 1 >= args.len() {
+                    println!("Invalid port was given !");
+                    exit(84);
+                }
+                settings.port = match args[i + 1].parse() {
+                    Ok(value) => value,
+                    Err(_) => {
+                        println!("Invalid port was given !");
+                        exit(84);
+                    }
+                };
+                i += 1;
+            }
+            "-i" | "--interval" => {
+                if i + 1 >= args.len() {
+                    println!("Invalid interval time was given !");
+                    exit(84);
+                }
+                if settings.d_flag {
+                    continue;
+                }
+                settings.interval = match args[i + 1].parse() {
+                    Ok(value) => value,
+                    Err(_) => {
+                        println!("Invalid interval time was given !");
+                        exit(84);
+                    }
+                };
+                i += 1;
+            }
+            _ => {
+                if settings.ip.is_empty() {
+                    settings.ip = &args[i];
+                } else {
+                    if let Some(char) = args[i].chars().next() {
+                        if char == '-' {
+                            println!("Unknown flag : {} !", args[i]);
+                            exit(84);
+                        }
+                    };
+                    println!("Invalid usage ! more than one target was given !");
+                    exit(84);
                 }
             }
         }
-        4 => {
-            settings.port = match args[3].parse() {
-                Ok(value) => value,
-                Err(_) => {
-                    println!("Invalid argument was given !");
-                    exit(84)
-                }
-            };
-            settings.interval = match args[2].parse() {
-                Ok(value) => value,
-                Err(_) => {
-                    println!("Invalid argument was given !");
-                    exit(84)
-                }
-            }
-        }
-        _ => {
-            println!("Usage: {} <host> [<interval> <port>]", args[0]);
-            exit(84)
-        }
+        i += 1;
     }
 }
