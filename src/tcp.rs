3a78e4a7b61ed78cf2f4ca2eda522f2be7a39d9b
Liloche19 <lilian.locher@epitech.eu>
2025-03-02 13:02:13 +0100

    [~] ping over TCP a little bit improved

--- src/tcp.rs
+++ src/tcp.rs
@@ -11,15 +11,15 @@ pub fn my_tcping(ip: &str, interval: u64, port: u16, results: &mut Vec<u128>) {
         .expect("Error setting Ctrl-C handler");
     let mut exit = false;
     println!(
-        "Pinging {} with {} ms of interval on port {}",
-        ip, interval, port
+        "Pinging {} on port {} with {} ms of interval",
+        ip, port, interval
     );
     while !exit {
         let mut stream =
             TcpStream::connect(format!("{}:{}", ip, port)).expect("Error connecting to address!");
+        let mut buffer: [u8; 1] = [0; 1];
         let now = Instant::now();
         stream.write_all(&[1]).unwrap();
-        let mut buffer: [u8; 1] = [0; 1];
         stream.read_exact(&mut buffer).unwrap();
         let elapsed = now.elapsed();
         results.push(elapsed.as_micros());
@@ -28,7 +28,7 @@ pub fn my_tcping(ip: &str, interval: u64, port: u16, results: &mut Vec<u128>) {
             ip,
             stream.peer_addr().unwrap(),
             port,
-            ((elapsed.as_micros() as u32) as f64) / 1000.0
+            (elapsed.as_micros() as f64) / 1000.0
         );
         if rx.recv_timeout(Duration::from_millis(interval)).is_ok() {
             exit = true;
